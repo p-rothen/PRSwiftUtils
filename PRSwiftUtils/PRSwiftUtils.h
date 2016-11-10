1d40fa3cbfed8cf141bb46fd7a35130676cc3b69
Pedro Rothen <p.rothen@outlook.cl>
2016-11-10 10:03:04 -0300

    Initial Commit

---
+++ PRSwiftUtils/PRSwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  PRSwiftUtils.h
+//  PRSwiftUtils
+//
+//  Created by Pedro on 10-11-16.
+//  Copyright © 2016 Rothen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PRSwiftUtils.
+FOUNDATION_EXPORT double PRSwiftUtilsVersionNumber;
+
+//! Project version string for PRSwiftUtils.
+FOUNDATION_EXPORT const unsigned char PRSwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PRSwiftUtils/PublicHeader.h>
+
+
